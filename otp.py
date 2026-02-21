9422964668f7822fb85634b8a47e0ca678907060
Manish Debnath <102667373+deadbeast38030@users.noreply.github.com>
2026-02-21 16:01:15 +0530

    otp.py

--- otp.py
+++ otp.py
@@ -3,29 +3,35 @@ import random
 
 
 # Page config (optional but makes it look better)
-st.set_page_config(page_title="OTP Generator", page_icon="🔐")
 
+st.set_page_config(page_title="OTP Generator", page_icon="🔐")
 st.title("🔐 OTP Generator by Manish")
 
 # Session_state to store OTP
-if "otp" not in st.session_state:#Storing the data as streamlit reloads the page everytime we click a button 
-    st.session_state.otp = None #if the otp doesnot exists it will be set to none 
+
+if "otp" not in st.session_state:                                #Storing the data as streamlit reloads the page everytime we click a button 
+    st.session_state.otp = None                                  #if the otp doesnot exists it will be set to none 
 
 # Generate OTP button
-if st.button("Generate OTP"):#if the button is pressed
-    st.session_state.otp = random.randint(1000, 9999)  # Generating a random 4-digit OTP
+
+if st.button("Generate OTP"):                                    #if the button is pressed
+    st.session_state.otp = random.randint(1000, 9999)            # Generating a random 4-digit OTP
     st.success("OTP Generated Successfully!")
+    
 # Display OTP
-if st.session_state.otp:#if the otp is not "none"
-    st.code(st.session_state.otp, language="text")#Display the otp in code style box
-    st.warning("⚠️ Please do not share your OTP with anyone.")#printing a warning message in yellow box
+
+if st.session_state.otp:                                          #if the otp is not "none"
+    st.code(st.session_state.otp, language="text")                #Display the otp in code style box
+    st.warning("⚠️ Please do not share your OTP with anyone.")    #printing a warning message in yellow box
 
     # Optional: Add verify section
-    user_input = st.text_input("Enter OTP to Verify")#printing a text to take input from the user the otp
+    
+    user_input = st.text_input("Enter OTP to Verify")              #printing a text to take input from the user the otp
 
-    if st.button("Verify OTP"):#if he clicks the verify otp button
-        if user_input == str(st.session_state.otp):#checking the user input with the random generated otp
+    if st.button("Verify OTP"):                                    #if he clicks the verify otp button
+        if user_input == str(st.session_state.otp):                #checking the user input with the random generated otp
             st.success("✅ OTP Verified Successfully!")
         else:
             st.error("❌ Incorrect OTP. Try again.")
+
 
